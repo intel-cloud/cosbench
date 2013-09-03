2e394933f306762d8cfc289b8e5d6f8b4eff79ef
jialinsun <jialinsun815@gmail.com>
2013-09-03 10:18:36 +0800

    add some new classed for loading

---
+++ dev/cosbench-controller/src/com/intel/cosbench/controller/loader/AbstractSnapshotLoader.java
@@ -0,0 +1,59 @@
+/** 
+ 
+Copyright 2013 Intel Corporation, All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. 
+ */
+
+package com.intel.cosbench.controller.loader;
+
+import java.io.*;
+
+import com.intel.cosbench.model.StageInfo;
+import com.intel.cosbench.model.WorkloadInfo;
+
+/**
+ * This class is the base class for exporting run information.
+ * 
+ * @author ywang19, qzheng7
+ * 
+ */
+abstract class AbstractSnapshotLoader implements SnapshotLoader{
+
+	protected BufferedReader reader;
+	protected WorkloadInfo workloadContext;
+	protected StageInfo stageContext;
+	
+	public AbstractSnapshotLoader() {
+		/* empty */
+	}
+	
+	@Override
+	public void init(BufferedReader reader, WorkloadInfo workloadContext,
+			String stageId) throws IOException {
+		this.reader = reader;
+		this.workloadContext = workloadContext;
+		this.stageContext = workloadContext.getStageInfo(stageId);
+	}
+
+	@Override
+	public void load() throws IOException{
+		readHeader();
+		readSnapshot();
+	}
+	
+	protected abstract void readHeader() throws IOException;
+
+	protected abstract void readSnapshot() throws IOException;
+
+}
---
+++ dev/cosbench-controller/src/com/intel/cosbench/controller/loader/SimpleWorkloadLoader.java
@@ -0,0 +1,135 @@
+package com.intel.cosbench.controller.loader;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.intel.cosbench.config.Stage;
+import com.intel.cosbench.config.WorkloadResolver;
+import com.intel.cosbench.config.XmlConfig;
+import com.intel.cosbench.config.castor.CastorConfigTools;
+import com.intel.cosbench.controller.model.StageContext;
+import com.intel.cosbench.controller.model.StageRegistry;
+import com.intel.cosbench.controller.model.WorkloadContext;
+import com.intel.cosbench.log.LogFactory;
+import com.intel.cosbench.log.Logger;
+import com.intel.cosbench.model.StageInfo;
+import com.intel.cosbench.model.StageState;
+import com.intel.cosbench.model.WorkloadInfo;
+import com.intel.cosbench.service.WorkloadLoader;
+
+public class SimpleWorkloadLoader implements WorkloadLoader {
+	private static final Logger LOGGER = LogFactory.getSystemLogger();
+
+	private static final File ROOT_DIR = new File("archive");
+
+	static {
+		if (!ROOT_DIR.exists())
+			ROOT_DIR.mkdirs();
+		String path = ROOT_DIR.getAbsolutePath();
+		LOGGER.info("using {} for loading workload archives", path);
+	}
+
+	public SimpleWorkloadLoader() {
+		/* empty */
+	}
+
+	private static String getRunDirName(WorkloadInfo info) {
+		String name = info.getId();
+		name += '-' + info.getWorkload().getName();
+		return name;
+	}
+
+	@Override
+	public List<WorkloadInfo> loadWorkloadRun() throws IOException {
+		File file = new File(ROOT_DIR, "run-history.csv");
+		if (!file.exists())
+			return null;
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		RunLoader loader = Loaders.newRunExporter(reader);
+		List<WorkloadInfo> workloads = new ArrayList<WorkloadInfo>();
+		workloads = loader.load();
+		if (reader != null)
+			reader.close();
+		return workloads;
+	}
+
+	@Override
+	public void loadWorkloadPageInfo(WorkloadInfo workloadContext)
+			throws IOException {
+		loadWorkloadConfig(workloadContext);
+		loadWorkloadFile(workloadContext);
+	}
+
+	private void loadWorkloadConfig(WorkloadInfo workloadContext)
+			throws FileNotFoundException {
+
+		File file = new File(
+				new File(ROOT_DIR, getRunDirName(workloadContext)),
+				"workload-config.xml");
+		if (!file.exists())
+			return;
+		XmlConfig config = new XmlConfig(new FileInputStream(file));
+		WorkloadResolver resolver = CastorConfigTools.getWorkloadResolver();
+		workloadContext.setWorkload(resolver.toWorkload(config));
+		createStages(workloadContext);
+	}
+
+	private void createStages(WorkloadInfo workloadContext) {
+		StageRegistry registry = new StageRegistry();
+		int index = 1;
+		for (Stage stage : workloadContext.getWorkload().getWorkflow()) {
+			String id = "s" + index++;
+			registry.addStage(createStageContext(id, stage));
+		}
+		((WorkloadContext) workloadContext).setStageRegistry(registry);
+	}
+
+	private static StageContext createStageContext(String id, Stage stage) {
+		StageContext context = new StageContext();
+		context.setId(id);
+		context.setStage(stage);
+		context.setState(StageState.COMPLETED, true);
+		return context;
+	}
+
+	private static String getWorkloadFileName(WorkloadInfo info) {
+		String name = info.getId();
+		name += "-" + info.getWorkload().getName();
+		return name;
+	}
+
+	private void loadWorkloadFile(WorkloadInfo workloadContext)
+			throws IOException {
+		File file = new File(
+				new File(ROOT_DIR, getRunDirName(workloadContext)),
+				getWorkloadFileName(workloadContext) + ".csv");
+		if (!file.exists())
+			return;
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		WorkloadFileLoader loader = Loaders.newWorkloadLoader(reader,
+				workloadContext);
+		loader.load();
+	}
+	
+    private static String getStageFileName(StageInfo info) {
+        String name = info.getId();
+        name += "-" + info.getStage().getName();
+        return name;
+    }
+    
+	@Override
+	public void loadStagePageInfo(WorkloadInfo workloadContext, String stageId)
+			throws IOException {
+		File file = new File(
+				new File(ROOT_DIR, getRunDirName(workloadContext)),
+				getStageFileName(workloadContext.getStageInfo(stageId))
+						+ ".csv");
+		if (!file.exists())
+			return;
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		SnapshotLoader loader = Loaders.newSnapshotLoader(reader, workloadContext, stageId);
+		loader.load();
+	}
+
+}
