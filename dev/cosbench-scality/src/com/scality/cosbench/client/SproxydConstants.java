67735dbfbade87038defb1a6ab61922fe2dea307
Christophe Vedel <cv@scality.com>
2013-10-02 14:09:55 +0200

    storage adaptor for Scality sproxyd

---
+++ dev/cosbench-scality/src/com/scality/cosbench/client/SproxydConstants.java
@@ -0,0 +1,39 @@
+/**
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
+package com.scality.cosbench.client;
+
+public interface SproxydConstants {
+
+	String LOGGING_KEY = "logging";
+
+	Boolean LOGGING_DEFAULT = Boolean.FALSE;
+
+	// --------------------------------------------------------------------------
+	// CONNECTION
+	// --------------------------------------------------------------------------
+
+	String CONN_TIMEOUT_KEY = "timeout";
+	int CONN_TIMEOUT_DEFAULT = 30000;
+
+	String BASE_PATH_KEY = "base_path";
+	String BASE_PATH_DEFAULT = "/proxy/chord";
+
+	String HOSTS_KEY = "hosts";
+	String HOSTS_DEFAULT = "127.0.0.1";
+
+	String PORT_KEY = "port";
+	int PORT_DEFAULT = 81;
+}
