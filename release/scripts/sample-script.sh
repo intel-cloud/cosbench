b838fc1c8a1d7c9943feef62aea5b2297174204b
Cxquan <838612286@qq.com>
2014-07-29 13:40:29 +0800

    update script-sample.sh

--- release/scripts/sample-script.sh
+++ release/scripts/sample-script.sh
@@ -4,25 +4,30 @@
 # 1. The trigger feature could be enabled by set trigger attribute in workload configuration file, 
 #      it should be like: trigger="<script_name>.sh(<arg1>,<arg2>,...)"
 # 2. Currently trigger attribute support <workload> and <stage> level
-# 3. Script files should be put in scripts/ directory
+# 3. Script files should be put in 'scripts/' directory
 # 4. Script writer should enable '-k' option for a script to kill all the sub process after a workload/stage finished
 # 5. User could see all script output logs in archive/<workload_name>/scripts.log after a workload finished
 
 
-dir=<directory_to_store_collected_data>
-df_run_time=300
-node_lst=<file_to_set_node_list>
+#directory to store collected data
+dir=/results
 
-if [ !-e $node_lst ]
+#default run time of iostat
+df_run_time=300
+
+#nodes array 
+nodes=(ceph01 ceph02 ceph03)
+
+
+if [ $cnt -le 0 ]
 then
-	echo "!!$node_lst doesn't exists!!"
-	echo "current working directory is `pwd`"
+	echo "!!nodes doesn't exists!!"
 fi
 
 if [ "$1" = "-k" ]; then
 	echo "==== stop collecting data===="
 	echo "All results will be saved to $dir"
-	for node in `cat $node_lst`
+	for node in ${nodes[*]}
 	do
 		echo "stopping iostat on $node..."
 		ssh ${node} "killall -9 iostat"
@@ -56,7 +61,7 @@ run_time=$1
 if [ ! $run_time ]; then run_time=$df_run_time ; fi
 
 echo "==== start collecting data ===="
-for node in `cat $node_lst`
+for node in ${nodes[*]}
 do 
 	echo "starting iostat on $node..."
 	ssh ${node} "iostat -dxm 1 ${run_time} > /opt/iostat.txt &"
