99e03387443a4f3768965b82814966e57883db87
Cxquan <838612286@qq.com>
2014-07-29 14:05:46 +0800

    add copyright to sample-script.sh

--- release/scripts/sample-script.sh
+++ release/scripts/sample-script.sh
@@ -1,6 +1,24 @@
 #/bin/bash
+#
+#Copyright 2013 Intel Corporation, All Rights Reserved.
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+#
 
-#    This is just a sample to show how to write trigger script.
+#-------------------------------
+# COSBENCH TRIGGER SCRIPT SAMPLE
+#-------------------------------
+
 # 1. The trigger feature could be enabled by set trigger attribute in workload configuration file, 
 #      it should be like: trigger="<script_name>.sh(<arg1>,<arg2>,...)"
 # 2. Currently trigger attribute support <workload> and <stage> level
