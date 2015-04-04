5a74eb3bc0fd13d4048af539a53eb3b21e40e685
ywang19 <yaguang.wang@intel.com>
2015-04-04 11:14:03 +0800

    fix a IllegalStateException from not thread-safe httpclient connection
    Signed-off-by: ywang19 <yaguang.wang@intel.com>

--- dev/cosbench-http/src/com/intel/cosbench/client/http/HttpClientUtil.java
+++ dev/cosbench-http/src/com/intel/cosbench/client/http/HttpClientUtil.java
@@ -43,6 +43,7 @@ import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.SingleClientConnManager;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
@@ -61,8 +62,6 @@ public class HttpClientUtil {
 
     /**
      * Creates a default HTTP client with a given timeout setting.<br />
-     * Note that this client is <strong>NOT Thread-Safe</strong> and can only be
-     * used by a single thread.
      * 
      * @param timeout
      *            the timeout in seconds that will be honored by this client
@@ -70,23 +69,21 @@ public class HttpClientUtil {
      */
     public static HttpClient createHttpClient(int timeout) {
     	// make it support self-signed certification for https.
-	      HttpParams params = createDefaultHttpParams(timeout);
-	      ClientConnectionManager cm = createClientConnManager();
-	
-	      return new DefaultHttpClient(cm, params);    	    	
-    }
-
-    private static  HttpParams createDefaultHttpParams(int timeout) {
         HttpParams params = new BasicHttpParams();
         /* default HTTP parameters */
         DefaultHttpClient.setDefaultHttpParams(params);
         /* connection/socket timeouts */
         HttpConnectionParams.setSoTimeout(params, timeout);
-        HttpConnectionParams.setConnectionTimeout(params, timeout);
-        
+        HttpConnectionParams.setConnectionTimeout(params, timeout);        
         /* user agent */
         HttpProtocolParams.setUserAgent(params, "cosbench/2.0");
-        return params;
+        
+	    SchemeRegistry sr = new SchemeRegistry();          
+		sr.register(new Scheme("http", 80, PlainSocketFactory.getSocketFactory()));
+		sr.register(new Scheme("https", 443, createSSLSocketFactory()));			
+		ClientConnectionManager cm = new ThreadSafeClientConnManager(sr);
+	
+	    return new DefaultHttpClient(cm, params);    	    	
     }
     
     @SuppressWarnings({ "deprecation"})
@@ -121,16 +118,6 @@ public class HttpClientUtil {
         } 	
     }
 
-    private static ClientConnectionManager createClientConnManager()
-    {
-        SchemeRegistry sr = new SchemeRegistry();            
-        
-        sr.register(new Scheme("http", 80, PlainSocketFactory.getSocketFactory()));
-        sr.register(new Scheme("https", 443, createSSLSocketFactory()));
-
-        return new SingleClientConnManager(sr);
-    }
-    
     /**
      * Releases the resources held by the given HTTP client.<br />
      * Note that no further connections can be made upon a disposed HTTP client.
