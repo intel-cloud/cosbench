35c2fcb6fc869b918626e65b842be008fc0890e8
ywang19 <yaguang.wang@intel.com>
2015-08-07 14:04:58 +0800

    make heartbeat can be reactivated when driver is restored
    Signed-off-by: ywang19 <yaguang.wang@intel.com>

--- dev/cosbench-controller/src/com/intel/cosbench/controller/service/PingDriverRunner.java
+++ dev/cosbench-controller/src/com/intel/cosbench/controller/service/PingDriverRunner.java
@@ -30,23 +30,49 @@ public class PingDriverRunner implements Runnable{
     protected static final Logger LOGGER = LogFactory.getSystemLogger();
 
 	private static final int base_interval = 20;	// heartbeat interval in seconds.
-	private static final int failure_tolerance = 15; // in total 5 mins.
+	private static final int failure_tolerance = 3; // how many failures in a row is tolerable.
 	private static final int ext_interval = 3*base_interval; // extended interval for the case failures is over tolerance.
 	private static final int delay = 3; // timeout for socket wait in seconds
 	private DriverInfo[] driverInfos;
+	private int driver_count = 0;
 	private int[] failures;
-	private boolean isHealthy = true;
+	private boolean[] all_alive;
 	
 	PingDriverRunner(DriverInfo[] driverInfos){
 		this.driverInfos = driverInfos;
-		this.failures = new int[this.driverInfos.length];
+		this.driver_count = driverInfos.length;
+		this.failures = new int[this.driver_count];
+		this.all_alive = new boolean[this.driver_count];
+	}
+	
+	public boolean isAlive() {
+		for(boolean status : all_alive) {
+			if(!status)
+				return false;
+		}
+		
+		return true;
+	}
+	
+	public void checkFaultyDrivers() {
+		for(int i=0; i<driver_count; i++) {
+			if(!all_alive[i]) {
+				LOGGER.error(driverInfos[i].getName() + " at " + driverInfos[i].getUrl());
+			}
+		}		
 	}
 	
 	@Override
 	public void run() {
 		while (true) {
 			pingDrivers(driverInfos);
-			int interval = isHealthy? base_interval: ext_interval;
+			int interval = base_interval;
+			if(!isAlive()) {
+				LOGGER.error("below drivers are not alive:");
+				checkFaultyDrivers();
+				interval = ext_interval;
+			}
+
 			try {
 				LOGGER.info("begin to sleep for " + interval + " seconds.");
 				Thread.sleep(interval*1000);
@@ -65,7 +91,7 @@ public class PingDriverRunner implements Runnable{
 			Integer port = getDriverPort(driver.getUrl());
 			LOGGER.info("Trying to ping driver " + driver.getName() + " at " + driver.getUrl() + " with ip=" + ipAddress + ", port=" + port + "...");
 			if (ipAddress == null || ipAddress.isEmpty()) { 
-				isHealthy = false;
+				all_alive[i] = false;
 				failures[i] = failure_tolerance;
 				LOGGER.error("the driver ip address shouldn't be empty, the heartbeat is disabled to driver !" + driver.getName());
 				return;
@@ -75,18 +101,18 @@ public class PingDriverRunner implements Runnable{
 			try{
 				socket = new Socket();
 				socket.connect(new InetSocketAddress(ipAddress, port), delay*1000);
-				isHealthy = true;
+				all_alive[i] = true;
 				failures[i] = 0;
 				LOGGER.info("The driver " + driver.getName() + " at " + driver.getUrl() + " is reachable");
 			}catch(Exception e){
 				failures[i]++;
 				LOGGER.warn("The driver " + driver.getName() + " at " + driver.getUrl() + " is not reachable at the " + failures[i] + " time, with error message: " + e.getMessage());
 				if(failures[i] >= failure_tolerance) {
-					isHealthy = false;
+					all_alive[i] = false;
 					LOGGER.warn("The driver " + driver.getName() + " at " + driver.getUrl() + " hits failure tolerance, will extend the heartbeat interval to " + ext_interval + " seconds");
 				}
 			}finally{
-				driver.setAliveState(isHealthy);
+				driver.setAliveState(all_alive[i]);
 				if (socket != null) {
 					try {
 						socket.close();
