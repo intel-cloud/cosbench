e6c5ceb9c320199bca6080387281218c793d96eb
Tatsuya Nanjo <noralife@gmail.com>
2015-12-01 22:25:13 +0900

    Adjust indent

--- dev/cosbench-gcs/src/com/intel/cosbench/api/GcsStor/GcsStorageFactory.java
+++ dev/cosbench-gcs/src/com/intel/cosbench/api/GcsStor/GcsStorageFactory.java
@@ -4,14 +4,14 @@ import com.intel.cosbench.api.storage.*;
 
 public class GcsStorageFactory implements StorageAPIFactory {
 
-    @Override
-    public String getStorageName() {
-        return "gcs";
-    }
+	@Override
+	public String getStorageName() {
+		return "gcs";
+	}
 
-    @Override
-    public StorageAPI getStorageAPI() {
-        return new GcsStorage();
-    }
+	@Override
+	public StorageAPI getStorageAPI() {
+		return new GcsStorage();
+	}
 
 }
